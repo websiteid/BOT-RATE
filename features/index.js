350ffcc032d314227cca0b9c5a86ea6ffa5e2ecc
websiteid <junimatalgano@gmail.com>
2026-03-12 18:41:57 +0700

    Update index.js

--- features/index.js
+++ features/index.js
@@ -6,27 +6,50 @@ function register(bot) {
   const files = fs.readdirSync(__dirname)
     .filter(file => file !== 'index.js' && file.endsWith('.js'));
 
+  const features = {};
+
   for (const file of files) {
-
     const feature = require(path.join(__dirname, file));
-
-    if (feature.command && feature.execute) {
-
-      bot.onText(new RegExp(`/${feature.command}`), (msg) => {
-        feature.execute(bot, msg);
-      });
-
+    if (feature.command) {
+      features[feature.command] = feature;
     }
-
-    if (feature.handleMessage) {
-
-      bot.on('message', (msg) => {
-        feature.handleMessage(bot, msg);
-      });
-
-    }
-
   }
+
+  bot.on('message', async (msg) => {
+
+    const text = msg.text;
+
+    if (!text) return;
+
+    // START
+    if (text === '/start') {
+      return features['start'].execute(bot, msg);
+    }
+
+    // HELP
+    if (text === '❓ Help') {
+      return features['help'].execute(bot, msg);
+    }
+
+    // MENFES
+    if (text === '💌 Menfes') {
+      return features['menfes'].execute(bot, msg);
+    }
+
+    // KIRIM PAP
+    if (text === '📤 Kirim PAP') {
+      return features['kirimpap'].execute(bot, msg);
+    }
+
+    // HANDLE SESSION FEATURE
+    for (const key in features) {
+      const feature = features[key];
+      if (feature.handleMessage) {
+        await feature.handleMessage(bot, msg);
+      }
+    }
+
+  });
 
 }
 
