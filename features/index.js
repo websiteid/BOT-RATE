0f89cac49cd0d81c2910331dde420268d71f1ceb
websiteid <junimatalgano@gmail.com>
2026-03-12 18:37:53 +0700

    Update index.js

--- features/index.js
+++ features/index.js
@@ -1,17 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-const features = {};
+function register(bot) {
 
-const files = fs.readdirSync(__dirname)
-  .filter(file => file !== 'index.js' && file.endsWith('.js'));
+  const files = fs.readdirSync(__dirname)
+    .filter(file => file !== 'index.js' && file.endsWith('.js'));
 
-for (const file of files) {
-  const feature = require(path.join(__dirname, file));
+  for (const file of files) {
 
-  if (feature.command) {
-    features[feature.command] = feature;
+    const feature = require(path.join(__dirname, file));
+
+    if (feature.command && feature.execute) {
+
+      bot.onText(new RegExp(`/${feature.command}`), (msg) => {
+        feature.execute(bot, msg);
+      });
+
+    }
+
+    if (feature.handleMessage) {
+
+      bot.on('message', (msg) => {
+        feature.handleMessage(bot, msg);
+      });
+
+    }
+
   }
+
 }
 
-module.exports = features;
+module.exports = { register };
